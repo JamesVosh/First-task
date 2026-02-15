c63203e3abf03f92db418221547c82ff0f8b2588
JamesVosh <dinislam.oraz15@gmail.com>
2026-02-16 00:02:48 +0300

    NUnit Test and Benchmark

--- Program.cs
+++ Program.cs
@@ -1,161 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace First_task
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using First_task;
+namespace Benchmark
 {
     class Program
     {
-        private static int i = 0;
-        private static string[] XYID;
-        private static Mat_tochki.Voditeli mas_vodit = new Mat_tochki.Voditeli();
-        private static Mat_tochki.Zakazi zakaz;
         static void Main()
         {
-            Console.WriteLine("Введите параметр размера сетки N: ");
-            mas_vodit.N = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите параметр размера сетки M: ");
-            mas_vodit.M = Convert.ToInt32(Console.ReadLine());
-            do
-            {
-                XYID = Console.ReadLine().Split(' ');
-                if (XYID.Length == 3)
-                {
-                    mas_vodit.Append(mas_vodit, new Mat_tochki(Convert.ToInt32(XYID[0]), Convert.ToInt32(XYID[1]), XYID[2]));
-                }
-                else
-                {
-                    if ((Convert.ToInt32(XYID[0]) >= 0 && Convert.ToInt32(XYID[0]) < mas_vodit.N) && (Convert.ToInt32(XYID[1]) >= 0 && Convert.ToInt32(XYID[1]) < mas_vodit.M))
-                    {
-                        zakaz = new Mat_tochki.Zakazi(Convert.ToInt32(XYID[0]), Convert.ToInt32(XYID[1]));
-                        break;
-                    }
-                    else throw new ArgumentOutOfRangeException();
-                }
-            }
-            while (true);
-            while (i < mas_vodit.Len(mas_vodit))
-            {
-                mas_vodit[i].X = mas_vodit[i].X - zakaz.X;
-                mas_vodit[i].Y = mas_vodit[i].Y - zakaz.Y;
-                i++;
-            }
-            mas_vodit.Sort_Shella(mas_vodit, mas_vodit.Len(mas_vodit));
-            i = 0;
-            while (i < 5)
-            {
-                Console.WriteLine(Convert.ToString(mas_vodit[i].X) + " " + Convert.ToString(mas_vodit[i].Y) + " " + Convert.ToString(mas_vodit[i].ID));
-                i++;
-            }
+            var summary = BenchmarkRunner.Run<FirstTaskBenchmark>();
         }
     }
-
-    public class Mat_tochki
+    [MemoryDiagnoser]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class FirstTaskBenchmark
     {
-        public string ID;
-        public int X, Y;
-        public Mat_tochki(int x, int y, string id)
+        private Mat_tochki.Voditeli vodit_bench = new Mat_tochki.Voditeli();
+        [GlobalSetup]
+        public void Setup()
         {
-            X = x;
-            Y = y;
-            ID = id;
-        }
-        public double Gipotenuza
-        {
-            get { return Math.Sqrt(Math.Pow(X, 2.0) + Math.Pow(Y, 2.0)); }
-        }
-        public class Voditeli
-        {
-            public int N;
-            public int M;
-            private List<Mat_tochki> voditel = new List<Mat_tochki>() { };
-            private int mini;
-            private Mat_tochki non;
-            public Mat_tochki this[int index]
+            Mat_tochki.Zakazi zakaz_bench = new Mat_tochki.Zakazi(5, 7);
+            int i = 0;
+            vodit_bench.N = 20; vodit_bench.M = 20;
+            vodit_bench.Append(vodit_bench, new Mat_tochki(0, 0, "v1"));
+            vodit_bench.Append(vodit_bench, new Mat_tochki(3, 1, "v2"));
+            vodit_bench.Append(vodit_bench, new Mat_tochki(2, 7, "v3"));
+            vodit_bench.Append(vodit_bench, new Mat_tochki(5, 3, "v4"));
+            vodit_bench.Append(vodit_bench, new Mat_tochki(11, 7, "v5"));
+            vodit_bench.Append(vodit_bench, new Mat_tochki(10, 1, "v6"));
+            vodit_bench.Append(vodit_bench, new Mat_tochki(9, 5, "v7"));
+            vodit_bench.Append(vodit_bench, new Mat_tochki(7, 10, "v8"));
+            vodit_bench.Append(vodit_bench, new Mat_tochki(4, 7, "v9"));
+            vodit_bench.Append(vodit_bench, new Mat_tochki(4, 6, "v10"));
+            while (i < vodit_bench.Len(vodit_bench))
             {
-                get
-                {
-                    if (index >= 0 && index < voditel.Count) return voditel[index];
-                    else throw new IndexOutOfRangeException();
-                }
-                set 
-                {
-                    voditel[index] = value;
-                }
-            }
-            public void Append(Voditeli mas, Mat_tochki point)
-            {
-                if ((point.X >= 0 && point.X < N) && (point.Y >= 0 && point.Y < M))
-                {
-                    mas.voditel.Add(point);
-                }
-                else
-                {
-                    throw new ArgumentOutOfRangeException("point.X or point.Y");
-                }
-            }
-            public int Len(Voditeli mas)
-            {
-                return mas.voditel.Count;
-            }
-            public List<Mat_tochki> Sort_direct_choice(Voditeli mas, int len)
-            {
-                for (int i = 0; i < len; i++)
-                {
-                    mini = i;
-                    for (int j = i; j < len - 1; j++)
-                    {
-                        if (mas.voditel[mini].Gipotenuza > mas.voditel[j + 1].Gipotenuza)
-                        {
-                            mini = j + 1;
-                        }
-                    }
-                    non = mas.voditel[i];
-                    mas.voditel[i] = mas.voditel[mini];
-                    mas.voditel[mini] = non;
-                }
-                return voditel;
-            }
-            public List<Mat_tochki> Sort_bubble(Voditeli mas, int len)
-            {
-                for (int i = 0; i < len; i++)
-                {
-                    for (int j = 0; j < (len - 1 - i); j++)
-                    {
-                        if (mas.voditel[j].Gipotenuza > mas.voditel[j + 1].Gipotenuza)
-                        {
-                            non = mas.voditel[j];
-                            mas.voditel[j] = mas.voditel[j + 1];
-                            mas.voditel[j + 1] = non;
-                        }
-                    }
-                }
-                return voditel;
-            }
-            public List<Mat_tochki> Sort_Shella(Voditeli mas, int len)
-            {
-                for (int s = len / 2; s > 0; s /= 2)
-                {
-                    for (int i = s; i < len; ++i)
-                    {
-                        for (int j = i - s; j >= 0 && mas.voditel[j].Gipotenuza > mas.voditel[j + s].Gipotenuza; j -= s)
-                        {
-                            non = mas.voditel[j];
-                            mas.voditel[j] = mas.voditel[j + s];
-                            mas.voditel[j + s] = non;
-                        }
-                    }
-                }
-                return voditel;
+                vodit_bench[i].X = vodit_bench[i].X - zakaz_bench.X;
+                vodit_bench[i].Y = vodit_bench[i].Y - zakaz_bench.Y;
+                i++;
             }
         }
-        public class Zakazi
+        [Benchmark]
+        public void Sort_Shella_10()
         {
-            public int X, Y;
-            public Zakazi(int x, int y)
-            {
-                X = x;
-                Y = y;
-            }
+            vodit_bench.Sort_Shella(vodit_bench, vodit_bench.Len(vodit_bench));
+        }
+        [Benchmark]
+        public void Sort_bubble_10()
+        {
+            vodit_bench.Sort_bubble(vodit_bench, vodit_bench.Len(vodit_bench));
+        }
+        [Benchmark]
+        public void Sort_direct_choice_10()
+        {
+            vodit_bench.Sort_direct_choice(vodit_bench, vodit_bench.Len(vodit_bench));
         }
     }
 }
---
+++ UnitTest1.cs
@@ -0,0 +1,188 @@
+using First_task;
+using NUnit.Framework;
+namespace FirstTaskTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Voditeli_Mat_tochki_negative_IndexOutOfRangeException()
+        {
+            var vodit_test = new Mat_tochki.Voditeli();
+            vodit_test.N = 20; vodit_test.M = 20;
+            vodit_test.Append(vodit_test, new Mat_tochki(0, 0, "v1"));
+            vodit_test.Append(vodit_test, new Mat_tochki(3, 1, "v2"));
+            vodit_test.Append(vodit_test, new Mat_tochki(19, 19, "v3"));
+            vodit_test.Append(vodit_test, new Mat_tochki(0, 19, "v4"));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[-1].X) + " " + Convert.ToString(vodit_test[0].Y) + " " + Convert.ToString(vodit_test[0].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[0].X) + " " + Convert.ToString(vodit_test[-1].Y) + " " + Convert.ToString(vodit_test[0].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[0].X) + " " + Convert.ToString(vodit_test[0].Y) + " " + Convert.ToString(vodit_test[-1].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[-1].X) + " " + Convert.ToString(vodit_test[-1].Y) + " " + Convert.ToString(vodit_test[-1].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[-10].X) + " " + Convert.ToString(vodit_test[0].Y) + " " + Convert.ToString(vodit_test[0].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[0].X) + " " + Convert.ToString(vodit_test[-10].Y) + " " + Convert.ToString(vodit_test[0].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[0].X) + " " + Convert.ToString(vodit_test[0].Y) + " " + Convert.ToString(vodit_test[-10].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[-10].X) + " " + Convert.ToString(vodit_test[-10].Y) + " " + Convert.ToString(vodit_test[-10].ID)));
+        }
+        [Test]
+        public void Voditeli_Mat_tochki_positive_IndexOutOfRangeException()
+        {
+            var vodit_test = new Mat_tochki.Voditeli();
+            vodit_test.N = 20; vodit_test.M = 20;
+            vodit_test.Append(vodit_test, new Mat_tochki(0, 0, "v1"));
+            vodit_test.Append(vodit_test, new Mat_tochki(3, 1, "v2"));
+            vodit_test.Append(vodit_test, new Mat_tochki(19, 19, "v3"));
+            vodit_test.Append(vodit_test, new Mat_tochki(0, 19, "v4"));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[4].X) + " " + Convert.ToString(vodit_test[0].Y) + " " + Convert.ToString(vodit_test[0].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[0].X) + " " + Convert.ToString(vodit_test[4].Y) + " " + Convert.ToString(vodit_test[0].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[0].X) + " " + Convert.ToString(vodit_test[0].Y) + " " + Convert.ToString(vodit_test[4].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[4].X) + " " + Convert.ToString(vodit_test[4].Y) + " " + Convert.ToString(vodit_test[4].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[10].X) + " " + Convert.ToString(vodit_test[0].Y) + " " + Convert.ToString(vodit_test[0].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[0].X) + " " + Convert.ToString(vodit_test[10].Y) + " " + Convert.ToString(vodit_test[0].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[0].X) + " " + Convert.ToString(vodit_test[0].Y) + " " + Convert.ToString(vodit_test[10].ID)));
+            Assert.Throws<IndexOutOfRangeException>(() => Console.WriteLine(Convert.ToString(vodit_test[10].X) + " " + Convert.ToString(vodit_test[10].Y) + " " + Convert.ToString(vodit_test[10].ID)));
+        }
+        [Test]
+        public void Voditeli_Mat_tochki_positive()
+        {
+            var vodit_test = new Mat_tochki.Voditeli();
+            vodit_test.N = 20; vodit_test.M = 20;
+            vodit_test.Append(vodit_test, new Mat_tochki(0, 0, "v1"));
+            vodit_test.Append(vodit_test, new Mat_tochki(3, 1, "v2"));
+            vodit_test.Append(vodit_test, new Mat_tochki(19, 19, "v3"));
+            vodit_test.Append(vodit_test, new Mat_tochki(0, 19, "v4"));
+            Console.WriteLine(Convert.ToString(vodit_test[0].X) + " " + Convert.ToString(vodit_test[0].Y) + " " + Convert.ToString(vodit_test[0].ID));
+            Console.WriteLine(Convert.ToString(vodit_test[1].X) + " " + Convert.ToString(vodit_test[1].Y) + " " + Convert.ToString(vodit_test[1].ID));
+            Console.WriteLine(Convert.ToString(vodit_test[2].X) + " " + Convert.ToString(vodit_test[2].Y) + " " + Convert.ToString(vodit_test[2].ID));
+            Console.WriteLine(Convert.ToString(vodit_test[3].X) + " " + Convert.ToString(vodit_test[3].Y) + " " + Convert.ToString(vodit_test[3].ID));
+            Assert.Pass();
+        }
+        [Test]
+        public void Voditeli_Append_positive_ArgumentOutOfRangeException()
+        {
+            var vodit_test = new Mat_tochki.Voditeli();
+            vodit_test.N = 20; vodit_test.M = 20;
+            Assert.Throws<ArgumentOutOfRangeException>(() => vodit_test.Append(vodit_test, new Mat_tochki(1, 20, "v3")));
+            Assert.Throws<ArgumentOutOfRangeException>(() => vodit_test.Append(vodit_test, new Mat_tochki(20, 1, "v3")));
+            Assert.Throws<ArgumentOutOfRangeException>(() => vodit_test.Append(vodit_test, new Mat_tochki(20, 20, "v3")));
+            Assert.Throws<ArgumentOutOfRangeException>(() => vodit_test.Append(vodit_test, new Mat_tochki(1, 100, "v3")));
+            Assert.Throws<ArgumentOutOfRangeException>(() => vodit_test.Append(vodit_test, new Mat_tochki(100, 1, "v3")));
+            Assert.Throws<ArgumentOutOfRangeException>(() => vodit_test.Append(vodit_test, new Mat_tochki(100, 100, "v3")));
+        }
+        [Test]
+        public void Voditeli_Append_negative_ArgumentOutOfRangeException()
+        {
+            var vodit_test = new Mat_tochki.Voditeli();
+            vodit_test.N = 20; vodit_test.M = 20;
+            Assert.Throws<ArgumentOutOfRangeException>(() => vodit_test.Append(vodit_test, new Mat_tochki(1, -19, "v3")));
+            Assert.Throws<ArgumentOutOfRangeException>(() => vodit_test.Append(vodit_test, new Mat_tochki(-1, 19, "v3")));
+            Assert.Throws<ArgumentOutOfRangeException>(() => vodit_test.Append(vodit_test, new Mat_tochki(-1, -19, "v3")));
+        }
+        [Test]
+        public void Voditeli_Append_positive()
+        {
+            var vodit_test = new Mat_tochki.Voditeli();
+            vodit_test.N = 20; vodit_test.M = 20;
+            vodit_test.Append(vodit_test, new Mat_tochki(0, 0, "v1"));
+            vodit_test.Append(vodit_test, new Mat_tochki(3, 1, "v2"));
+            vodit_test.Append(vodit_test, new Mat_tochki(19, 19, "v3"));
+            vodit_test.Append(vodit_test, new Mat_tochki(0, 19, "v4"));
+            Assert.Pass();
+        }
+        [Test]
+        public void Voditeli_direct_choice_10()
+        {
+            int i = 0;
+            var vodit_test = new Mat_tochki.Voditeli();
+            vodit_test.N = 20; vodit_test.M = 20;
+            var zakaz_test = new Mat_tochki.Zakazi(5, 7);
+            vodit_test.Append(vodit_test, new Mat_tochki(0, 0, "v1"));
+            vodit_test.Append(vodit_test, new Mat_tochki(3, 1, "v2"));
+            vodit_test.Append(vodit_test, new Mat_tochki(2, 7, "v3"));
+            vodit_test.Append(vodit_test, new Mat_tochki(5, 3, "v4"));
+            vodit_test.Append(vodit_test, new Mat_tochki(11, 7, "v5"));
+            vodit_test.Append(vodit_test, new Mat_tochki(10, 1, "v6"));
+            vodit_test.Append(vodit_test, new Mat_tochki(9, 5, "v7"));
+            vodit_test.Append(vodit_test, new Mat_tochki(7, 10, "v8"));
+            vodit_test.Append(vodit_test, new Mat_tochki(4, 7, "v9"));
+            vodit_test.Append(vodit_test, new Mat_tochki(4, 6, "v10"));
+            while (i < vodit_test.Len(vodit_test))
+            {
+                vodit_test[i].X = vodit_test[i].X - zakaz_test.X;
+                vodit_test[i].Y = vodit_test[i].Y - zakaz_test.Y;
+                i++;
+            }
+
+            vodit_test.Sort_direct_choice(vodit_test, vodit_test.Len(vodit_test));
+
+            Assert.That(vodit_test[0].ID, Is.EqualTo("v9"));
+            Assert.That(vodit_test[1].ID, Is.EqualTo("v10"));
+            Assert.That(vodit_test[2].ID, Is.EqualTo("v3"));
+            Assert.That(vodit_test[3].ID, Is.EqualTo("v8"));
+            Assert.That(vodit_test[4].ID, Is.EqualTo("v4"));
+        }
+        [Test]
+        public void Voditeli_bubble_10()
+        {
+            int i = 0;
+            var vodit_test = new Mat_tochki.Voditeli();
+            vodit_test.N = 20; vodit_test.M = 20;
+            var zakaz_test = new Mat_tochki.Zakazi(5, 7);
+            vodit_test.Append(vodit_test, new Mat_tochki(0, 0, "v1"));
+            vodit_test.Append(vodit_test, new Mat_tochki(3, 1, "v2"));
+            vodit_test.Append(vodit_test, new Mat_tochki(2, 7, "v3"));
+            vodit_test.Append(vodit_test, new Mat_tochki(5, 3, "v4"));
+            vodit_test.Append(vodit_test, new Mat_tochki(11, 7, "v5"));
+            vodit_test.Append(vodit_test, new Mat_tochki(10, 1, "v6"));
+            vodit_test.Append(vodit_test, new Mat_tochki(9, 5, "v7"));
+            vodit_test.Append(vodit_test, new Mat_tochki(7, 10, "v8"));
+            vodit_test.Append(vodit_test, new Mat_tochki(4, 7, "v9"));
+            vodit_test.Append(vodit_test, new Mat_tochki(4, 6, "v10"));
+            while (i < vodit_test.Len(vodit_test))
+            {
+                vodit_test[i].X = vodit_test[i].X - zakaz_test.X;
+                vodit_test[i].Y = vodit_test[i].Y - zakaz_test.Y;
+                i++;
+            }
+
+            vodit_test.Sort_bubble(vodit_test, vodit_test.Len(vodit_test));
+
+            Assert.That(vodit_test[0].ID, Is.EqualTo("v9"));
+            Assert.That(vodit_test[1].ID, Is.EqualTo("v10"));
+            Assert.That(vodit_test[2].ID, Is.EqualTo("v3"));
+            Assert.That(vodit_test[3].ID, Is.EqualTo("v8"));
+            Assert.That(vodit_test[4].ID, Is.EqualTo("v4"));
+        }
+        [Test]
+        public void Voditeli_Shella_10()
+        {
+            int i = 0;
+            var vodit_test = new Mat_tochki.Voditeli();
+            vodit_test.N = 20; vodit_test.M = 20;
+            var zakaz_test = new Mat_tochki.Zakazi(5, 7);
+            vodit_test.Append(vodit_test, new Mat_tochki(0, 0, "v1"));
+            vodit_test.Append(vodit_test, new Mat_tochki(3, 1, "v2"));
+            vodit_test.Append(vodit_test, new Mat_tochki(2, 7, "v3"));
+            vodit_test.Append(vodit_test, new Mat_tochki(5, 3, "v4"));
+            vodit_test.Append(vodit_test, new Mat_tochki(11, 7, "v5"));
+            vodit_test.Append(vodit_test, new Mat_tochki(10, 1, "v6"));
+            vodit_test.Append(vodit_test, new Mat_tochki(9, 5, "v7"));
+            vodit_test.Append(vodit_test, new Mat_tochki(7, 10, "v8"));
+            vodit_test.Append(vodit_test, new Mat_tochki(4, 7, "v9"));
+            vodit_test.Append(vodit_test, new Mat_tochki(4, 6, "v10"));
+            while (i < vodit_test.Len(vodit_test))
+            {
+                vodit_test[i].X = vodit_test[i].X - zakaz_test.X;
+                vodit_test[i].Y = vodit_test[i].Y - zakaz_test.Y;
+                i++;
+            }
+
+            vodit_test.Sort_Shella(vodit_test, vodit_test.Len(vodit_test));
+
+            Assert.That(vodit_test[0].ID, Is.EqualTo("v9"));
+            Assert.That(vodit_test[1].ID, Is.EqualTo("v10"));
+            Assert.That(vodit_test[2].ID, Is.EqualTo("v3"));
+            Assert.That(vodit_test[3].ID, Is.EqualTo("v8"));
+            Assert.That(vodit_test[4].ID, Is.EqualTo("v4"));
+        }
+    }
+}
